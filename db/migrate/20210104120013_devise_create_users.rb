0214408bdb954a0ec6b28e750e360f66a5621f13
KatsujiOgawa <marug711@gmail.com>
2021-01-04 21:52:49 +0900

    update: user登録＆ログインの設定変更

--- db/migrate/20210104120013_devise_create_users.rb
+++ db/migrate/20210104120013_devise_create_users.rb
@@ -6,6 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :name,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
@@ -36,7 +37,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
-    # add_index :users, :email,                unique: true
+    add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
